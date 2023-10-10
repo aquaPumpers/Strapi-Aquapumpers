8e19e10b9e81e0aeb1bb89bc58c577afbc04786f
Daria Ostapenko <145284707+aquaPumpers@users.noreply.github.com>
2023-10-10 15:24:44 +0000

    +

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aquapumpers.com',
-  integrations: [mdx(), sitemap(), react(), tailwind()]
+  integrations: [mdx(), sitemap(), react(), tailwind({
+    applyBaseStyles: false,
+  }),]
 });
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,15 +1,12 @@
 /** @type {import("prettier").Config} */
-module.exports = {
-    // i am just using the standard config, change if you need something else
-    ...require('prettier-config-standard'),
-    pluginSearchDirs: [__dirname],
-    plugins: [require.resolve('prettier-plugin-astro')],
-    overrides: [
+export default {
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss' ],
+  overrides: [
       {
-        files: '*.astro',
-        options: {
-          parser: 'astro'
-        }
-      }
-    ]
-  }
+          files: '*.astro',
+          options: {
+              parser: 'astro',
+          },
+      },
+  ],
+};
